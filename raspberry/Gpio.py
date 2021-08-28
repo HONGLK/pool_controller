d6284c6472136bafef506844ad82f37990640b7f
Lambert Chen <lambertchen@dps.com.tw>
2021-08-28 22:11:42 +0800

    gpio test

--- raspberry/Gpio.py
+++ raspberry/Gpio.py
@@ -1,10 +1,60 @@
 from datetime import datetime
 import RPi.GPIO as gp
+import json
 
-Relay = {
-    "Relay_Ch1" : 26,
-    "Relay_Ch2" : 20,
-    "Relay_Ch3" : 21
-}
+relay = {
+    "1" : 26,
+    "2" : 20,
+    "3" : 21
+    }
 
-GPIO.setwarnings(Relay)
+
+gp.setwarnings(False)
+gp.setmode(gp.BCM)
+
+gp.setup(relay["1"], gp.OUT)
+gp.setup(relay["2"], gp.OUT)
+gp.setup(relay["3"], gp.OUT)
+
+class message_obj():
+    def __init__(self, event, status, message):
+        self.time = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
+        self.event = event
+        self.status = status
+        self.message = message
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=False, indent=4)
+
+def relay_operation(channel, opt_status):
+    #0 = off, 1 = on
+    try:
+        if(channel in [1, 2, 3] and opt_status != gp.input(relay[str(channel)])):
+            if(opt_status == 0): #opt_status = 0代表想關閉
+                relay_opt = gp.LOW
+                gp.ouput(relay[str(channel)], relay_opt)
+                current_satus = gp.input(relay[str(channel)])
+
+                if(current_satus == opt_status):
+                    msg = message_obj("relay_operaion", "0000", "改變狀態成功，請檢查繼電器")
+                    print(msg.toJSON())
+                else:
+
+                    msg = message_obj("relay_operaion", "0001", "改變狀態失敗，請檢查繼電器")
+                    print(msg.toJSON())
+
+            elif(opt_status == 1): #opt_status = 1代表想開啟
+                relay_opt = gp.HIGH
+                gp.ouput(relay[str(channel)], relay_opt)
+                current_satus = gp.input(relay[str(channel)])
+                if(current_satus == opt_status):
+                    msg = message_obj("relay_operaion", "0000", "改變狀態成功，請檢查繼電器")
+                    print(msg.toJSON())
+                else:
+                    msg = message_obj("relay_operaion", "0001", "改變狀態失敗，請檢查繼電器")
+                    print(msg.toJSON())
+        else:
+            msg = message_obj("relay_operaion", "0002", "輸入參數錯誤，請檢查")
+    except:
+        msg = message_obj("relay_operaion", "1111", "Exception請檢查!")
