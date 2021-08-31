9fa777c77911f9024f8018c4559ea284065a0ad8
Lambert <lambertchen@dps.tw>
2021-08-31 23:05:43 +0800

    test

--- server.py
+++ server.py
@@ -104,6 +104,6 @@ def handle_response(data):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 33507))
     socketio.run(app, host='0.0.0.0', debug=True, port=port)
     
